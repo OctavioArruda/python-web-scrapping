368ff3ede3ea490ee06c40b33590da981aca0a3e
Octavio Arruda <oneiros241@gmail.com>
2020-12-09 21:16:47 -0300

    :zap: scrapping infos and formatting

--- main.py
+++ main.py
@@ -13,4 +13,18 @@ with open('home.html', 'r') as html_file:
     for course in courses_html_tags:
         print(course.text) # Printing the content of each h5 tag, that represents the course name
 
+    # Searching for the "Python for beginners" course then grabbing it's price
+    course_cards = soup.find_all('div', class_='card') # Grab all the div's with the card class
+
+    for course in course_cards:
+        # print(course.h5)
+        course_name = course.h5.text
+        course_price = course.a.text.split()[-1] # Spliting by blank spaces and grabbing the last part
+
+        # print(course_name)
+        # print(course_price)
+        
+        print(f'{course_name} costs {course_price}') # Displaying a very nice info about each of the courses
+
+
     
